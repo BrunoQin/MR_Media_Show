8f92e713d2a0e34f8c3edc146fedd6843a9275e6
杜盛瑀 <772773671@qq.com>
2017-10-28 00:14:12 +0800

    首页模拟滑轮滚动文字效果

--- app/js/introductionController.js
+++ app/js/introductionController.js
@@ -5,6 +5,7 @@
 //主页的初始化
 var introductionController = function(){
 
+    //设置背景视频的大小
     function configVideo(){
         window.onresize = function () {
             resizeToCover();
@@ -35,14 +36,58 @@ var introductionController = function(){
         $('#video_contanier').scrollLeft((new_height - height) / 2);
     }
 
+    //展示出当前的页面
     function showPage(){
         $("#container > div").hide();
         $("#introduction").show();
     };
 
+    //设置星球大战文字动画
+    function setStartScroll(){
+        var controller = new ScrollMagic.Controller({container:"#wrapper"});
+        var textShow = TweenMax.staggerFromTo("#wrapper .item",1,
+            {
+                scale:0.8,
+                opacity:0
+            },
+            {
+                scale:1.5,
+                opacity:1
+            },
+            0.2)
+        var sceneShow = new ScrollMagic.Scene({
+            triggerElement:"#trigger",
+            duration:300,
+            offset: 100
+        })
+            .setTween(textShow)
+            .addIndicators()
+            .addTo(controller);
+
+        var textHide = TweenMax.staggerFromTo("#wrapper .item",1,
+            {
+                scale:1.5,
+                opacity:1
+            },
+            {
+                scale:3,
+                opacity:0
+            },
+            0.2)
+        var sceneHide = new ScrollMagic.Scene({
+            triggerElement:"#trigger",
+            duration:200,
+            offset: 500
+        })
+            .setTween(textHide)
+            .addIndicators()
+            .addTo(controller);
+    }
+
     var controller = {};
     var execute = function () {
         configVideo();
+        setStartScroll();
         showPage();
     }
     controller.execute = execute;
