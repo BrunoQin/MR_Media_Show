23f4be2057c6f483d5d8cbfeba388c9db8b29863
杜盛瑀 <772773671@qq.com>
2017-10-27 20:25:32 +0800

    首页背景图片实时屏幕整个浏览器窗口

--- app/js/introductionController.js
+++ app/js/introductionController.js
@@ -4,11 +4,47 @@
  */
 //主页的初始化
 var introductionController = function(){
-    var showPage = function(){
+    
+    function configVideo(){
+        window.onresize = function () {
+            resizeToCover();
+        };
+        $(window).trigger("resize");
+    }
+
+    //背景视频图片实时平铺整个界面
+    function resizeToCover(){
+        var width = $(document).width();
+        var height = $(document).height();
+
+        var video_height = $("#video").attr("height");
+        var video_width = $("#video").attr("width");
+
+        var scale_h = height/video_height;
+        var scale_w = width/video_width;
+
+        var scale = scale_w > scale_h ? scale_w : scale_h;
+
+        var new_height = scale * video_height;
+        var new_width = scale * video_width
+
+        $("#video").attr("width",new_width);
+        $("#video").attr("height",new_height);
+
+        $('#video_contanier').scrollLeft((new_width - width) / 2);
+        $('#video_contanier').scrollLeft((new_height - height) / 2);
+    }
+
+    function showPage(){
         $("#container > div").hide();
         $("#introduction").show();
     };
+
     var controller = {};
-    controller.execute = showPage;
+    var execute = function () {
+        configVideo();
+        showPage();
+    }
+    controller.execute = execute;
     return controller;
 }
